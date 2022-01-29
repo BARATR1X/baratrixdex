3a775940754236577c06ce86620cbb9109823aef
João Luis Vieira Escorcio <92925231+BARATR1X@users.noreply.github.com>
2022-01-28 22:47:41 -0300

    Add files via upload

--- js/script1.js
+++ js/script1.js
@@ -1,23 +1,22 @@
-const getPokemonUrl = (id) => `http://pokeapi.co/api/v2/pokemon/${id}`;  // atribuindo uma função para o id da url 
+const getPokemonUrl = (id) => `http://pokeapi.co/api/v2/pokemon/${id}`;
 
 const generatePokemonPromises = () =>
-  Array(151)  // reserva de arrays de quantidade de pokemons na geração
+  Array(151)
     .fill()
-    .map((_, index) =>  fetch(getPokemonUrl(index + 1)) .then((response) => response.json()) //
+    .map((_, index) =>
+      fetch(getPokemonUrl(index + 1)).then((response) => response.json())
     );
 
 const generateHTML = (pokemons) =>
   pokemons.reduce((accumulator, { name, id, types }) => {
     const elementTypes = types.map((typeinfo) => typeinfo.type.name);
 
-    const index = id < 10 ? "00" : "";  // está atribuindo 2 numeros 0 na frente do numero da dex de cada pokemon até o pokemon numeros menores 9
+    const index = id < 10 ? "00" : "";
 
-    const indexTwo = id > 9 && id < 100 ? "0" : ""; // está atribuindo 1 numero 0 na frente do numero da dex de cada pokemon até o pokemon entre os numeros 10 e 99
+    const indexTwo = id > 9 && id < 100 ? "0" : "";
 
-
-    //adidcionando os cards de cada pokemon
     accumulator += `
-      <li class="card ${elementTypes[0]}">  
+      <li class="card ${elementTypes[0]}">
         <img class="card-image " alt="${name}" src="https://cdn.traction.one/pokedex/pokemon/${id}.png"/>
         <h2 class="card-title">${index}${indexTwo}${id}. ${name.toUpperCase()}</h2>
         <p class="card-subtitle">${elementTypes.join(" | ").toUpperCase()}</p> 
